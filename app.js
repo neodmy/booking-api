263e409950247288d299a1f4c4699fd51e1dddb4
David Yusta <david.yusta@guidesmiths.com>
2020-08-27 09:56:45 +0200

    feat: add basic graphql schemas, query and mutation

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+    `),
+  rootValue: {
+    events: () => ['Romantic Cooking', 'Sailing', 'All-Night Coding'],
+    createEvent: (args) => {
+      const { name } = args;
+      return name;
+    },
+  },
+  graphiql: true,
+}));
 
 app.listen(3000);
