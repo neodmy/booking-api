14971025c4bde67dbb298f6779d346703668bee4
David Yusta <david.yusta@guidesmiths.com>
2020-08-28 23:57:05 +0200

    feat: add Access-Control to pass Cors

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  return next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
